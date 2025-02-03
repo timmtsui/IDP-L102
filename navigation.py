7daf51e8aabe4f7cab7fea03a7631bdac065ee6c
L-hoffmann <lh841@cam.a.uk>
2025-02-03 11:58:39 +0000

    Nav file

---
+++ navigation.py
@@ -0,0 +1,16 @@
+# 5x6 matrix which stores information about the physical course
+# Each element in the matrix is an array which encodes the junction directions in [N,E,S,W] respectively
+
+# [0,0] is the top left corner (furthest North East)
+# Positive x is defined as West
+# Positive y is defined as South
+
+Nav_Grid = [
+    [[0,1,1,0] , [0,1,0,1] , [0,1,1,1] , [0,1,1,1] , [0,0,1,1]],
+    [[1,0,1,0] , [0,1,0,0] , [1,0,1,1] , [1,0,0,0] , [1,0,1,0]],
+    [[1,1,1,0] , [0,1,0,1] , [1,1,0,1] , [0,1,1,1] , [1,0,1,1]],
+
+
+
+]
+
